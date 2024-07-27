cfa20195e3c78ef4758bb7ca0b63ae0f6b6f8c04
AyonC <ayon@github.com>
2024-07-27 12:12:51 +0530

    Add: footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="flex justify-center border-primary/20 border-t-2 p-5">
-      &copy; NuxeCorps 24'
+      &copy; NuxeCorps 24&quot;
     </div>
   );
 }
